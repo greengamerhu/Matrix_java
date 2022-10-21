8b4ceab7ff5157a9792541456d510cb545d34672
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-10-21 11:17:46 +0200

    s

--- src/hu/petrik/matrixoop/Main.java
+++ src/hu/petrik/matrixoop/Main.java
@@ -1,2 +1,19 @@
-package hu.petrik.matrixoop;public class Main {
+package hu.petrik.matrixoop;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix m1 = new Matrix(5,5);
+        System.out.println(m1);
+        System.out.println();
+        Matrix m2 = new Matrix(5,5);
+        System.out.println(m2);
+        System.out.printf("Mátrix elemeinek osszege: %d ", m2.getElemekOsszege() );
+        System.out.println();
+        System.out.printf("Mátrix pozitív elemeinek szama: %d",m2.getPozitivElemekSzama() );
+        System.out.println();
+        System.out.printf("Mátrix Legnagyobb elem: %d",m2.getLegnagyobbElem() );
+
+    }
 }
--- src/hu/petrik/matrixoop/Matrix.java
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -1,2 +1,88 @@
-package hu.petrik.matrixoop;public class Matrix {
-}
+package hu.petrik.matrixoop;
+
+import java.util.Random;
+
+public class Matrix {
+    private int sor;
+    private int oszlop;
+    private int[][] m;
+    private static final Random rnd = new Random();
+
+    public Matrix(int sor, int oszlop) {
+        this.sor = sor;
+        this.oszlop = oszlop;
+        this.m = new int[sor][oszlop];
+        for (int i = 0; i < this.m.length; i++) {
+            for (int j = 0; j < this.m[i].length; j++) {
+                this.m[i][j] = rnd.nextInt(199)-99;
+            }
+        }
+    }
+    public int getElemekOsszege() {
+        int osszeg = 0;
+        for (int[] sor : this.m) {
+            for(int elem : sor) {
+                osszeg += elem;
+            }
+        }
+        return osszeg;
+    }
+    public int getPozitivElemekSzama () {
+        int db = 0;
+        for (int[] sor : this.m) {
+            for(int elem : sor) {
+                if (elem > 0) {
+                    db++;
+                }
+            }
+        }
+        return db;
+    }
+    public int getLegnagyobbElem() {
+        int legnagyobb = this.m[0][0];
+
+        for (int i = 0; i < this.m[0].length; i++) {
+            for (int j = 0; j < this.m.length; j++) {
+                if (legnagyobb < this.m[j][i]) {
+                    legnagyobb = this.m[j][i];
+                }
+            }
+        }
+        return  legnagyobb;
+
+    }
+    public int[] findElsoPozitiv() {
+        int sorIndex = 0;
+        int oszlopIndex = 0;
+        while (sorIndex < this.m.length && this.m[sorIndex][oszlopIndex] < 1) {
+
+            while(oszlopIndex < this.m[sorIndex].length && this.m[sorIndex][oszlopIndex] < 1) {
+                oszlopIndex++;
+            }
+            if (oszlopIndex == this.m[sorIndex].length) {
+                oszlopIndex = 0;
+                sorIndex++;
+            }
+        }
+        if (sorIndex == this.m.length) {
+            sorIndex = -1;
+            oszlopIndex = -1;
+        }
+        return new int[] {sorIndex, oszlopIndex};
+    }
+     @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.m.length; i++) {
+            for (int j = 0; j < this.m[i].length; j++) {
+                sb.append(String.format("%3d", this.m[i][j]));
+            }
+            if (i < this.m.length - 1) {
+                sb.append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
+    }
+    }
+
+
